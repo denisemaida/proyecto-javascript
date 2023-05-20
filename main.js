52d3ae6df73f9e059b33a38aea39c1c400896896
Denise Maida <denimaida.97@gmail.com>
2023-05-19 22:00:43 -0300

    Se categorizaron los productos del index en el main de javascript, y se hizo una pequeña modificación en el index

---
+++ main.js
@@ -0,0 +1,217 @@
+const productosArray = [
+{  id: "hombres",
+    titulo: "Camisa roja Aéro",
+    imagen: "./pictures/camisas/camisa_roja_lisa_hombre.jpg",
+    categoria: {
+        nombre: "Hombres",
+        id: "hombres",
+    },
+    precio: 1000,
+},
+{ id: "mujeres",
+titulo: "Blusa off shoulder",
+imagen: "./pictures/camisas/blusa_estampada_flores_mujer.jpg",
+categoria: {
+    nombre: "Mujeres",
+    id: "mujeres",
+},
+precio: 2000,
+},
+{id: "mujeres",
+titulo: "Blusa fucsia",
+imagen: "./pictures/camisas/blusa_fucsia_mujer.png",
+categoria: {
+    nombre: "Mujeres",
+    id:"mujeres",
+},
+precio: 2000,
+},
+{id: "mujeres",
+titulo: "Camisa bordó",
+imagen: "./pictures/camisas/camisa_bordo_entallada_mujer.jpg",
+categoria: {
+    nombre: "Mujeres",
+    id: "mujeres",
+},
+precio: 1000,
+},
+{id: "niños",
+titulo: "Camisa manga corta Jean",
+imagen: "./pictures/camisas/camisa_corta_jean_niño.jpg",
+categoria: {
+    nombre: "Niños",
+    id: "niños",
+},
+precio: 500,},
+{id: "hombres",
+titulo: "camisa escosesa celeste",
+imagen: "./pictures/camisa_escosesa_celeste.jpg",
+categoria: {
+    nombre: "Hombres",
+    id: "hombres",
+}, precio: 1500,},{ id: "niños",
+titulo: "Camisa Bennetton niña",
+imagen: "./pictures/camisas/camisa_estampada_niña.jpg",
+categoria: {
+    nombre: "Niños",
+    id: "niños",
+}, precio: 500,
+},{
+    id: "niños",
+titulo: "Camisa Grisino dinos",
+imagen: "./pictures/camisas/camsisa_estampada_niño.jpg",
+categoria: {
+    nombre: "Niños",
+    id: "niños",
+}, precio: 500,
+},{ id: "niños",
+titulo: "Camisa blanca Zara",
+imagen: "./pictures/camisas/camisa_lisa_niña.jpg",
+categoria: {
+    nombre: "Niños",
+    id: "niños",
+}, precio: 500,},{
+    id:"hombres",
+    titulo: "Camisa manga corta Montagne",
+    imagen: "./pictures/camisas/camisa_manga_corta_hombre.jpg",
+categoria: {
+    nombre: "Hombres",
+    id: "hombres",
+}, precio: 1000,},
+{id: "niños",
+titulo: "Camisa manga corta amarilla",
+imagen: "./pictures/camisas/camisa_manga_corta_niña.jpeg",
+categoria: {
+nombre: "Niños",
+id:"niños",
+}, precio: 500,},
+{id: "hombres",
+titulo: "Buzo Aéro",
+imagen: "./pictures/abrigos/buzo_estampado_hombre.jpg",
+categoria: {
+nombre: "Hombres",
+id:"hombres",
+}, precio: 1800,},
+{id: "niños",
+titulo: "Buzo Spread",
+imagen: "./pictures/abrigos/buzo_estampado_niño.jpg",
+categoria: {
+    nombre:"Niños",
+    id: "niños", 
+}, precio: 900,
+},{
+    id: "hombres",
+    titulo: "Buzo Negro",
+    imagen: "./pictures/abrigos/buzo_liso_hombre.jpg",
+    categoria: {
+        nombre: "Hombres",
+        id: "hombres",
+}, precio: 1000,},{
+    id:"niños",
+    titulo: "Buzo bicolor Montagne",
+    imagen: "./pictures/abrigos/buzo_niño_deportivo.jpg",
+    categoria: {
+        nombre: "Niños",
+        id: "niños",
+}, precio: 900,},{
+id: "niños",
+titulo: "Buzo corto Pili",
+imagen: "./pictures/abrigos/buzos_cortos_niñas",
+categoria: {
+    nombre: "Niños",
+    id: "niños",
+}, precio: 900,},{
+    id: "niños",
+    titulo: "Buzo niña Happy",
+    imagen: "./pictures/abrigo/buzos_estampados_niñas.png",
+    categoria: {
+        nombre: "Niños",
+        id:"niños",
+    }, precio: 900,},{
+        id: "mujeres",
+titulo:"Buzo canguro colores",
+imagen: "./pictures/abrigos/buzos_lisos_mujer.jpeg",
+categoria: {
+    nombre: "Mujeres",
+id: "mujeres",}, precio: 1800},{
+    id: "hombres",
+    titulo: "Campera de abrigo Max",
+    imagen: "./pictures/abrigos/campera_abrigo_hombre.jpg",
+    categoria: {
+        nombre: "Hombres",
+        id:"hombres",}, precio: 2000,},
+{id:"mujeres",
+titulo: "Campera de abrigo Barbie",
+imagen: "./pictures/abrigos/campera_abrigo_mujer.jpg",
+categoria: {
+    nombre: "Mujeres",
+    id: "mujeres",}, precio: 2000,},{
+id: "niños",
+titulo: "Campera de abrigo Alex",
+imagen: "./pictures/abrigos/campera_abrigo_unisex_niños.jpg",
+categoria: {
+    nombre: "Niños",
+    id: "niños",}, precio: 1400,},
+{id: "mujeres",
+titulo: "Campera deportiva Adidas",
+imagen: "./pictures/abrigos/campera_adidas_deportiva_mujer.jpg",
+categoria: {
+    nombre: "Mujeres",
+    id: "mujeres",},precio: 1600,},
+{id :"hombres",
+titulo: "Campera deportiva negra",
+imagen: "./pictures/abrigos/campera_deportiva_hombre.jpg",
+categoria: {
+    nombre: "Hombres",
+    id: "hombres",
+}, precio: 1600,},{
+id:"mujeres",
+titulo: "Sweater bordado Flor",
+imagen: "./pictures/abrigos/sweater_bordado_mujer.jpg",
+categoria:{
+    nombre: "Mujeres",
+    id: "mujeres",}, precio: 2000,},
+{id: "hombres",
+titulo: "Sweater Negro",
+imagen: "./pictures/abrigos/sweater_liso_hombre.jpg",
+categoria: {
+    nombre: "Hombres",
+    id: "hombres",},precio: 1900,},{
+        id:"mujeres",
+        titulo: "Sweater Netflix",
+        imagen: "./pictures/abrigos/sweater_liso_mujer.jpg",
+        categoria: {
+            nombre: "Mujeres",
+            id: "mujeres,"}, precio: 1900,},
+{ id: "mujeres",
+titulo: "Sweater rayado Cindy",
+imagen: "./pictures/abrigos/sweater_rayado_mujer.jpg",
+categoria: {
+    nombre: "Mujeres",
+    id: "mujeres",}, precio: 1850,},{
+id: "hombres",
+titulo: "Sweater rombos",
+imagen: "./pictures/abrigos/sweater_rombos_hombre.jpg",
+categoria: {
+    nombre: "Hombres",
+    id: "hombres,"}, precio: 1850,},
+{id:"bebés",
+titulo: "Baberos estampados",
+imagen: "./pictures/accesorios/baberos_bebe.jpeg",
+categoria:{
+nombre: "Bebés",
+id: "bebés",}, precio: 100,},
+{id: "mujeres",
+titulo: "Bandolera verde Studio F",
+imagen: "./pictures/accesorios/bandolera_mujer.jpg",
+categoria: {
+    nombre:"Mujeres",
+    id: "mujeres",}, precio: 200,},
+{id: "hombres",
+    titulo: "Cinturon negro",
+    imagen: "./pictures/accesorios/cinturon_hombre.jpg",
+categoria: {
+    nombre: "Hombres",
+    id: "hombres",}, precio: 550,},
+]
+
