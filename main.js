b7e7bab9f59e1db62fbc6b7320c94eb64b505274
Denise Maida <denimaida.97@gmail.com>
2023-05-25 17:43:21 -0300

    Se agregaron cosas al main de javascript

--- main.js
+++ main.js
@@ -626,7 +626,15 @@ productos.forEach(productos => {
 
     const div = document.createElement("div");
 div.classList.add("producto");
-div.innerHTML = 
+div.innerHTML =  ` <img class="producto-imagen" src="${producto.imagen}"
+alt="${producto.titulo}">
+<div class="producto-detalles">
+<h3 class="producto-titulo">${producto.titulo}</h3>
+<p class="producto-precio">${producto.precio}</p>
+<button class="producto-agregar" id="${producto.id}">Agregar</button>
+</div>´;
+contenedorProductos.append(div);
+})
 
-});
 }
+cargarProductos();
